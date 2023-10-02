56edc7437983f15a1abdf071e1d9ca4df6aa6729
Eyasir Ahamed <eyasir329@gmail.com>
2023-10-02 21:40:29 +0600

    rigged

---
+++ codeforce/1879A_Rigged!.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+
+	int t{0};
+	cin>>t;
+	for(int i{0};i<t;i++){
+		int n{0},w{0},s{0},e{0},E{0},temp{0};
+		cin>>n;
+		for(int j=0;j<n;j++){
+			cin>>s>>e;
+			if(j==0){
+				w=s;
+				E=e;
+			}
+
+			if(e>=E){
+				if(s>=w && j!=0){
+					temp=1;
+				}
+			}
+
+			if(temp==1){
+				cout<<"-1"<<endl;
+			}else{
+				if(j==n-1){
+					cout<<w<<endl;
+				}
+			}
+		}		
+	}
+
+	cout<<endl;
+	return 0;
+}
