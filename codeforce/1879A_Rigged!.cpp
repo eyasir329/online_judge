df2e1ab789dbadf61e6b7a8a0465b709bd89816f
Eyasir Ahamed <eyasir329@gmail.com>
2023-10-09 16:33:00 +0600

    riped

--- codeforce/1879A_Rigged!.cpp
+++ codeforce/1879A_Rigged!.cpp
@@ -1,42 +1,70 @@
-#include<bits/stdc++.h>
+/*
+	=======বিসমিল্লাহির রাহমানির রাহিম=======
+	Author : Eyasir Ahamed
+	Sheikh Hasina University, Netrokona
+	===================================
+*/
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
+#define int long long int
 
-	#ifndef ONLINE_JUDGE
-		freopen("input.txt","r",stdin);
-		freopen("output.txt","w",stdout);
-	#endif
+void solve()
+{
+	int n{0}, w{0}, s{0}, e{0}, E{0}, temp{0};
+	cin >> n;
+	for (int j = 0; j < n; j++)
+	{
+		cin >> s >> e;
+		if (j == 0)
+		{
+			w = s;
+			E = e;
+		}
 
-	int t{0};
-	cin>>t;
-	for(int i{0};i<t;i++){
-		int n{0},w{0},s{0},e{0},E{0},temp{0};
-		cin>>n;
-		for(int j=0;j<n;j++){
-			cin>>s>>e;
-			if(j==0){
-				w=s;
-				E=e;
+		if (e >= E && j != 0)
+		{
+			if (s >= w)
+			{
+				temp = 1;
 			}
+		}
 
-			if(e>=E && j!=0){
-				if(s>=w){
-					temp=1;
-				}
+		if (temp == 1 && j == n - 1)
+		{
+			cout << "-1" << endl;
+		}
+		else
+		{
+			if (j == n - 1)
+			{
+				cout << w << endl;
 			}
+		}
+	}
+}
 
-			if(temp==1 && j==n-1){
-				cout<<"-1"<<endl;
-			}else{
-				if(j==n-1){
-					cout<<w<<endl;
-				}
-			}
-		}		
+int32_t main()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	cin >> t;
+	while (t--)
+	{
+		solve();
 	}
 
-	cout<<endl;
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+	cout << endl;
+
 	return 0;
 }
