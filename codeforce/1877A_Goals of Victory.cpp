91463e15de4a005edfbea5469ae1c44766e234ea
Eyasir Ahamed <eyasir329@gmail.com>
2023-12-16 01:02:08 +0600

    16 dec 23

---
+++ codeforce/1877A_Goals of Victory.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+#define pp pop_back
+#define vi vector <int>
+#define vii vector <pair<int,int>>
+#define si set <int>
+#define sii set <pair<int,int>>
+#define vc vector <char>
+#define all(a) a.begin(),a.end()
+#define sz(a) ((int) a.size())
+#define tos(x) to_string(x);
+#define sin(x) static_cast<int>(x)-48;
+#define print(a) for(auto x:a) cout<<x<<" ";cout<<endl
+#define endl '\n'
+
+void solve()
+{
+	int n,x,ct=0;
+	cin>>n;
+	for(int i=0;i<n-1;i++){
+		cin>>x;
+		ct+=x;
+	}
+	if(ct>0){
+		cout<<"-"<<ct<<endl;
+	}else{
+		cout<<abs(ct)<<endl;
+	}
+}
+
+int32_t main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    clock_t z = clock();
+    ll t = 1,i=1;
+    cin >> t;
+    while (t--){
+        auto s = ((double)(clock() - z) / CLOCKS_PER_SEC);
+        solve();
+        auto e = ((double)(clock() - z) / CLOCKS_PER_SEC);
+        cerr<<"Case # "<<i<<" Run Time "<<e-s<<endl;
+        i++;
+    }
+    return 0;
+}
+
+
+/*===============================================
+  :::::::::::::Author :Eyasir Ahamed:::::::::::::
+  :::::::::::::::eyasir329.com:::::::::::::::::::
+  ===============================================*/
