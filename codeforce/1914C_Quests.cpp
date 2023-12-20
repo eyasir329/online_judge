6ca9fe8b6cef55a37ca4c8147da78169d1a1277b
Eyasir Ahamed <eyasir329@gmail.com>
2023-12-20 21:25:56 +0600

    1914C

---
+++ codeforce/1914C_Quests.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define pb push_back
+#define pp pop_back
+#define vi vector <int>
+#define vii vector <pair<int,int>>
+#define si set <int>
+#define sii set <pair<int,int>>
+#define vc vector <char>
+#define all(a) a.begin(),a.end()
+#define sz(a) ((int) a.size())
+#define tos(x) to_string(x);
+#define sin(x) static_cast<int>(x)-48;
+#define print(a) for(auto x:a) cout<<x<<" ";cout<<endl
+#define endl '\n'
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vi a(n, 0), b(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    int a1=0,b1=0,ans=0;
+    for(int i=0;i<n;i++){
+        if(k==i){
+            break;
+        }
+        a1=a1+a[i];
+        b1=max(b1,b[i]);
+        ans=max(ans,a1+(k-(i+1))*b1);
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    clock_t z = clock();
+    ll t = 1,i=1;
+    cin >> t;
+    while (t--){
+        auto s = ((double)(clock() - z) / CLOCKS_PER_SEC);
+        solve();
+        auto e = ((double)(clock() - z) / CLOCKS_PER_SEC);
+        cerr<<"Case # "<<i<<" Run Time "<<e-s<<endl;
+        i++;
+    }
+    return 0;
+}
+
+
+/*===============================================
+  :::::::::::::Author :Eyasir Ahamed:::::::::::::
+  :::::::::::::::eyasir329.com:::::::::::::::::::
+  ===============================================*/
